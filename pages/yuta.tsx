d1c0071287f716d0b8dfe33d08b8f790df1fcb3f
y-matano <mata.nono.729@gmail.com>
2023-02-15 01:19:29 +0900

    yuta作成

--- pages/yuta.tsx
+++ pages/yuta.tsx
@@ -1,39 +1,37 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 
-const Yuta: NextPage = () => {
+const Yuta:NextPage = () => {
     return (
             <div className="bg-white py-6 sm:py-8 lg:py-12">
                 <div className="max-w-screen-md px-4 md:px-8 mx-auto">
-                <h1 className="text-gray-800 text-2xl sm:text-3xl font-bold text-center mb-4 md:mb-6">Our competitive advantage</h1>
+                <h1 className="text-gray-800 text-2xl sm:text-3xl font-bold text-center mb-4 md:mb-6">又野裕太 / Yuta Matano</h1>
             
                 <p className="text-gray-500 sm:text-lg mb-6 md:mb-8">
-                    This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated. It may be used to display a sample of fonts or generate text for testing. Filler text is dummy text which has no meaning however looks very similar to real text. The important factor when using filler text is that the text looks realistic otherwise it will not look very good.<br /><br />
+                    こちらは又野裕太の自己紹介用、テストホームページです。
             
-                    This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is <a href="#" className="text-indigo-500 hover:text-indigo-600 active:text-indigo-700 underline transition duration-100">random</a> or otherwise generated. It may be used to display a sample of fonts or generate text for testing. Filler text is dummy text which has no meaning however looks very similar to real text.
+                    
                 </p>
             
-                <h2 className="text-gray-800 text-xl sm:text-2xl font-semibold mb-2 md:mb-4">About us</h2>
+                <h2 className="text-gray-800 text-xl sm:text-2xl font-semibold mb-2 md:mb-4">About me</h2>
             
-                <p className="text-gray-500 sm:text-lg mb-6 md:mb-8">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated. It may be used to display a sample of fonts or generate text for testing. Filler text is dummy text which has no meaning however looks very similar to real text.</p>
+                <p className="text-gray-500 sm:text-lg mb-6 md:mb-8">又野裕太と申します。現在は東京大学経済学部に所属しており、学生経友会という組織とFITTY626というフットサルのサークルに所属しています。学問的な関心は数理統計学、ベイズ統計学及び機械学習とその周辺にあります。また、経済史、科学哲学といった分野にも関心があります。現在のところ、学部卒の時点での就職はせずに、大学院へ進学して、第一の関心分野である統計学の学習及び研究を行いたいと考えています。</p>
             
                 <ul className="list-disc list-inside text-gray-500 sm:text-lg mb-6 md:mb-8">
-                    <li>This is a section of some simple filler text</li>
-                    <li>Also known as placeholder text</li>
-                    <li>It shares some characteristics of a real written text</li>
+                    <p>経歴</p>
+                    <li>2018.4-2021.3 大阪府立茨木高校</li>
+                    <li>2021.4-2023.3 東京大学文科二類</li>
+                    <li>2023.3- 東京大学経済学部</li>
                 </ul>
             
-                <blockquote className="text-gray-500 sm:text-lg italic border-l-4 pl-4 md:pl-6 mb-6 md:mb-8">“This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated.”</blockquote>
+                <blockquote className="text-gray-500 sm:text-lg italic border-l-4 pl-4 md:pl-6 mb-6 md:mb-8">小中高でサッカーをしており、大学ではフットサルを行っています。好きなチームはガンバ大阪と、マンチェスターユナイテッドです。サッカー以外のスポーツも含めてスポーツ観戦が好きです！</blockquote>
             
-                <div className="bg-gray-100 overflow-hidden rounded-lg shadow-lg relative mb-6 md:mb-8">
-                    <img src="https://images.unsplash.com/photo-1593508512255-86ab42a8e620?auto=format&q=75&fit=crop&w=600&h=350" loading="lazy" alt="Photo by Minh Pham" className="w-full h-full object-cover object-center" />
-                </div>
             
-                <h2 className="text-gray-800 text-xl sm:text-2xl font-semibold mb-2 md:mb-4">Features</h2>
+                <h2 className="text-gray-800 text-xl sm:text-2xl font-semibold mb-2 md:mb-4">YEConnectでやりたいこと</h2>
             
-                <p className="text-gray-500 sm:text-lg">This is a section of some simple filler text, also known as placeholder text. It shares some characteristics of a real written text but is random or otherwise generated. It may be used to display a sample of fonts or generate text for testing. Filler text is dummy text which has no meaning however looks very similar to real text.</p>
+                <p className="text-gray-500 sm:text-lg">エンジニアリングを学ぶ。特に統計学を学習した後にテック企業でエンジニアリングを使うことができるような知識を身につける。</p>
                 </div>
             </div>
     )
     }
+
+export default Yuta
