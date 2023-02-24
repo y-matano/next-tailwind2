ae6d70eb6cbde067369d88269bed324ad61d4b9e
y-matano <mata.nono.729@gmail.com>
2023-02-24 20:46:17 +0900

    ボタンのComponent可

--- components/button.tsx
+++ components/button.tsx
@@ -18,16 +18,3 @@ return (
 )
 }
 export default Button;
-
-function Button0({ color, children }) {
-  const colorVariants = {
-    blue: 'bg-blue-600 hover:bg-blue-500',
-    red: 'bg-red-600 hover:bg-red-500',
-  }
-
-  return (
-    <button className={`${colorVariants[color]} ...`}>
-      {children}
-    </button>
-  )
-}
